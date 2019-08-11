78d60cb0f717e8f435ac59aaec1fca98237578a0
Юрків Назар <yurkiww@Urkivs-MacBook-Air.local>
2019-08-11 13:54:30 +0300

    r forms

--- src/modules/account-settings/account-settings.module.ts
+++ src/modules/account-settings/account-settings.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccountSettingsComponent } from 'src/pages/account-settings/account-settings.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AccountSettingsComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ReactiveFormsModule]
 })
-export class AccountSettingsModule { }
+export class AccountSettingsModule {}
--- src/pages/account-settings/account-settings.component.ts
+++ src/pages/account-settings/account-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account-settings',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.scss']
 })
 export class AccountSettingsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
+  form: FormGroup;
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required)
+    });
   }
-
+  onSubmit() {
+    console.log('Submitted', this.form);
+  }
 }
