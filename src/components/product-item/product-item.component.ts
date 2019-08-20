45cab89916b23f8844273036b07db19608d5b075
YarkoSumyk <yarko.sumyk@gmail.com>
2019-08-20 14:48:09 +0300

    Fixed sidebar and header for all pages, product item

--- src/components/product-item/product-item.component.ts
+++ src/components/product-item/product-item.component.ts
@@ -8,13 +8,7 @@ import { IProductItem } from 'src/interfaces/product-item';
 })
 export class ProductItemComponent implements OnInit {
   @Input() item: IProductItem;
-  @Output() open: EventEmitter<any> = new EventEmitter();
-  @Output() close: EventEmitter<any> = new EventEmitter();
-  visible: boolean = false;
-  onItem = () => {
-    this.visible = !this.visible;
-    this.visible ? this.open.emit(null) : this.close.emit(null);
-  };
+
   constructor() {}
 
   ngOnInit() {}
--- src/modules/modal/modal.module.ts
+++ src/modules/modal/modal.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from 'src/components/modal/modal.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [ModalComponent],
 })
 export class ModalModule {}
