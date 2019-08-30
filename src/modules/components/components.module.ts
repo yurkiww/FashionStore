0adb7fdff00813f9805e62c68b3b38c12a135acb
Maksym <maksicheg@gmail.com>
2019-08-30 16:09:23 +0300

    Created Add-item btn and new tab

--- src/modules/components/components.module.ts
+++ src/modules/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProductItemComponent } from 'src/components/product-item/product-item.component';
 import { SearchBoxComponent } from 'src/components/search-box/search-box.component';
 import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from 'src/components/button/button.component';
 
 @NgModule({
-  declarations: [ProductItemComponent, SearchBoxComponent],
+  declarations: [ProductItemComponent, SearchBoxComponent, ButtonComponent],
   imports: [CommonModule, FormsModule],
-  exports: [ProductItemComponent, SearchBoxComponent],
+  exports: [ProductItemComponent, SearchBoxComponent, ButtonComponent],
 })
 export class ComponentsModule {}
