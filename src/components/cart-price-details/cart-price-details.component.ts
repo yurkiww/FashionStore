76e5cb905fd9c69c56001664a187a07cb61c23b9
Maksym <maksicheg@gmail.com>
2019-10-02 09:58:57 +0300

    Fix

--- src/components/cart-price-details/cart-price-details.component.ts
+++ src/components/cart-price-details/cart-price-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { IPriceDetails } from 'src/interfaces/cart-price-details';
 
 @Component({
@@ -9,7 +9,21 @@ import { IPriceDetails } from 'src/interfaces/cart-price-details';
 export class CartPriceDetailsComponent implements OnInit {
   @Input() price: IPriceDetails;
   @Input() total: number;
+
+  @Output() OnOpen = new EventEmitter();
+  @Output() OnClose = new EventEmitter();
+  visible = false;
+
+  private element: any;
   constructor() {}
-  openModal() {}
+  openModal = () => {
+    this.visible = !this.visible;
+    this.visible ? this.OnOpen.emit() : this.OnClose.emit();
+  };
+  closeModal() {
+    this.element.style.display = 'none';
+    //document.body.classList.remove('jw-modal-open');
+  }
+
   ngOnInit() {}
 }
--- src/components/modal-place-order/modal-place-order.component.ts
+++ src/components/modal-place-order/modal-place-order.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { CartPriceDetailsComponent } from '../cart-price-details/cart-price-details.component';
+import { StorageService } from 'src/services/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-modal-place-order',
@@ -7,7 +10,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./modal-place-order.component.scss'],
 })
 export class ModalPlaceOrderComponent implements OnInit {
-  constructor(private fb: FormBuilder) {}
-
+  constructor(private storageService: StorageService) {}
+  closeModal() {
+    this.storageService.deleteToken();
+  }
   ngOnInit() {}
 }
